4f2b7edb386d9019af1eeee57235a32df5ec0eb0
discodancing <these.ten.minutes@gmail.com>
2020-08-25 14:10:25 -0700

    Add files via upload

---
+++ distortion.js
@@ -0,0 +1,10 @@
+var myAnimation = new hoverEffect({
+	parent: document.querySelector('[data-section-id="5f1874b38304ad336775b4ec"].page-section > div.section-background'),
+	intensity: 0.4,
+	image1: 'https://static1.squarespace.com/static/5f187409db749f75b4b70872/t/5f334c8129fb54274fc1e0ae/1597197443448/bg.png',
+	image2: 'https://static1.squarespace.com/static/5f187409db749f75b4b70872/t/5f334c8129fb54274fc1e0ae/1597197443448/bg.png',
+	displacementImage: 'https://static1.squarespace.com/static/5f187409db749f75b4b70872/t/5f4574c5fecac24f5a0a4226/1598387397524/displacement.png',
+	imagesRatio: 0.70,
+});
+
+
